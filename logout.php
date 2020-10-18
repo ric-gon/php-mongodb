a24b61e42ad89ae0e996916c63739acc77c143ab
ric-gon <71800542+ric-gon@users.noreply.github.com>
2020-10-18 17:45:10 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
   session_start();
+  /*
+  if (!isset($_SESSION['valid'])){
+    $SESSION['success'] = "Try again";
+    header("location: index.php");
+  }
+  */
+
   unset($_SESSION["user"]);
   unset($_SESSION["password"]);
 
